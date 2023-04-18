c73d46c2141c2c5725c596750b9ac19ba45f4431
Mateusz Kupidura <m.kupidura@gazeta.pl>
2023-04-18 15:11:13 +0200

    Przygotowanie kodu do kolejnego etapu - uczenie sieci neuronowych

--- program/main.py
+++ program/main.py
@@ -76,13 +76,27 @@ def unpickle_all(fname):
 #pickle_all(DATA_FOLDER+"parkinson_total_data.bin", [X_train, X_test, X_val, y_train[:,1], y_test[:,1], y_val[:,1]])
 
 #Gas_Turbine
-data = np.loadtxt(RAW_DATA_FOLDER+"propulsion_plants.txt")
-X = normalize(data[:,:-2], norm="max", axis=0)
-Y = data[:,-2:]
-X_train, X_test_val, y_train, y_test_val = train_test_split(X, Y, train_size=0.6)
-X_test, X_val, y_test, y_val = train_test_split(X_test_val, y_test_val, test_size=0.25)
-pickle_all(DATA_FOLDER+"GT_compressor_data.bin", [X_train, X_test, X_val, y_train[:,0], y_test[:,0], y_val[:,0]])
-pickle_all(DATA_FOLDER+"GT_turbine_data.bin", [X_train, X_test, X_val, y_train[:,1], y_test[:,1], y_val[:,1]])
+#data = np.loadtxt(RAW_DATA_FOLDER+"propulsion_plants.txt")
+#X = normalize(data[:,:-2], norm="max", axis=0)
+#Y = data[:,-2:]
+#X_train, X_test_val, y_train, y_test_val = train_test_split(X, Y, train_size=0.6)
+#X_test, X_val, y_test, y_val = train_test_split(X_test_val, y_test_val, test_size=0.25)
+#pickle_all(DATA_FOLDER+"GT_compressor_data.bin", [X_train, X_test, X_val, y_train[:,0], y_test[:,0], y_val[:,0]])
+#pickle_all(DATA_FOLDER+"GT_turbine_data.bin", [X_train, X_test, X_val, y_train[:,1], y_test[:,1], y_val[:,1]])
+
+
+data = ["rice", "anuran_family", "anuran_genus", "anuran_species", "dry_bean",
+        "electrical_grid", "parkinson_motor", "parkinson_total", "GT_compressor", "GT_turbine"] #nazwy plików zbiorów danych
+
+data_number = 0 #numer danych, na których będzie aktualne uruchomienie programu
+
+
+#ODCZYT DANYCH Z PLIKU .BIN
+[X_train, X_test, X_val, y_train, y_test, y_val] = unpickle_all(DATA_FOLDER+f"{data[data_number]}_data.bin")
+
+
+#PRZYGOTOWANIE SIECI
+#...
 
 
    
