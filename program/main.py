b3c58b1666ae95771e05d3caae2fad404cd7d01d
Mateusz Kupidura <m.kupidura@gazeta.pl>
2023-05-13 12:03:53 +0200

    Badania dla zbioru electrical_grid

--- program/main.py
+++ program/main.py
@@ -93,7 +93,7 @@ data = ["rice", "anuran_family", "anuran_genus", "anuran_species", "dry_bean",
         "electrical_grid", "parkinson_motor", "parkinson_total", "GT_compressor", "GT_turbine"] #nazwy plików zbiorów danych
 networks_neurons = [(30), (18,15), (16,13,10)]
 
-data_number = 4 #numer danych, na których będzie aktualne uruchomienie programu
+data_number = 5 #numer danych, na których będzie aktualne uruchomienie programu
 network_number = 0 #numer architektury sieci, dla której będzie aktualne uruchomienie programu (tylko dla uczenia)
 
 
@@ -141,248 +141,68 @@ methods = {'SP':prune.simple_pruning, 'SPA':prune.simple_pruning_amendment, 'KP'
           'PD':prune.PD_pruning, 'PEB':prune.PEB_pruning}
 
 ##dla klasyfikacji
-for network_number in range(3): #pętla po architekturach sieci
-    print("Architektura", networks_neurons[network_number])
-    l_n = str(networks_neurons[network_number]) if type(networks_neurons[network_number]) == int else '-'.join(np.array(networks_neurons[network_number], dtype=str))
-    [clf] = unpickle_all(NETWORK_FOLDER+f"{data[data_number]}_network_{l_n}.bin") #odczyt sieci z pliku .bin
-
-    for met in methods:
-        for los in [0, 0.025, 0.05, 0.075, 0.1]:
-            clf_t = copy.deepcopy(clf)
-            dele, f1_p, t_p = methods[met](clf_t, los, X_train, y_train, X_v=X_val, y_v=y_val, ep=50)
-            pickle_all(PRUNE_NET_FOLDER+f"{data[data_number]}_network_{l_n}_pruned_{met}_los_{los}", [clf_t])
-            print(met, los, dele, f1_p, t_p)
-            t_mean = 0 #średni czas predykcji
-            for _ in range(25):
-                t1 = time.time()
-                y_pred = clf_t.predict(X_test)
-                t_mean += (time.time()-t1)
-            t_mean = t_mean/25
-            f1_t = f1_score(y_test, y_pred, average='macro')
-            acc = accuracy_score(y_test, y_pred)
-            #kolejność kolumn: metoda przycinania, max utrata f1, czas przycinania, f1 przed douczaniem, usunięte połączenia, usunięte neurony, dokładność po przycinaniu, f1 po przycinaniu, czas predykcji po przycinaniu
-            if met in ['SP', 'SPA', 'KP', 'PBV']:
-                row = f"{met}; {los*100}%; {t_p}; {np.round(f1_p,3)}; {dele[0]}; {dele[1]}; {np.round(acc,3)}; {np.round(f1_t,3)}; {np.round(t_mean,6)}s \n"
-            else:
-                row = f"{met}; {los*100}%; {t_p}; {np.round(f1_p,3)}; -; {dele}; {np.round(acc,3)}; {np.round(f1_t,3)}; {np.round(t_mean,6)}s \n"
-            f = open(RESULT_FOLDER+f"raw_txt/{data[data_number]}_network_{l_n}.txt", 'a')
-            f.write(row)
-            f.close()
-            #generowanie macierzy konfuzji
-            cls_names = clf_t.class_labels_ #nazwy klas w zbiorze
-            conf_matrix = pd.DataFrame(confusion_matrix(y_test, y_pred, labels=cls_names), index=cls_names, columns=cls_names)
-            column_format = "|c|" + "".join(["c"]*conf_matrix.shape[0]) + "|"
-            conf_matrix.to_latex(RESULT_FOLDER+f"conf_matrix/{data[data_number]}_network_{l_n}_pruned_{met}_los_{los}.txt", column_format=column_format)
-
-##dla regresji
 #for network_number in range(3): #pętla po architekturach sieci
-#    print("Architektura", network_number)
+#    print("Architektura", networks_neurons[network_number])
 #    l_n = str(networks_neurons[network_number]) if type(networks_neurons[network_number]) == int else '-'.join(np.array(networks_neurons[network_number], dtype=str))
-#    [reg] = unpickle_all(NETWORK_FOLDER+f"{data[data_number]}_network_{l_n}.bin") #odczyt sieci z pliku .bin
+#    [clf] = unpickle_all(NETWORK_FOLDER+f"{data[data_number]}_network_{l_n}.bin") #odczyt sieci z pliku .bin
 
 #    for met in methods:
-#        if met == 'APERTP': #metoda działa identycznie do APERT dla regresji, więc zostaje pominięta
-#            continue
-#        for los in [0, 0.5, 1, 1.75, 2.5]:
-#            reg_t = copy.deepcopy(reg)
-#            dele, MSE_p, t_p = methods[met](reg_t, los, X_train, y_train, X_v=X_val, y_v=y_val, ep=50)
-#            pickle_all(PRUNE_NET_FOLDER+f"{data[data_number]}_network_{l_n}_pruned_{met}_los_{los}", [reg_t])
-#            print(met, los, dele, MSE_p, t_p)
+#        for los in [0, 0.025, 0.05, 0.075, 0.1]:
+#            clf_t = copy.deepcopy(clf)
+#            dele, f1_p, t_p = methods[met](clf_t, los, X_train, y_train, X_v=X_val, y_v=y_val, ep=50)
+#            pickle_all(PRUNE_NET_FOLDER+f"{data[data_number]}_network_{l_n}_pruned_{met}_los_{los}", [clf_t])
+#            print(met, los, dele, f1_p, t_p)
 #            t_mean = 0 #średni czas predykcji
 #            for _ in range(25):
 #                t1 = time.time()
-#                y_pred = reg_t.predict(X_test)
+#                y_pred = clf_t.predict(X_test)
 #                t_mean += (time.time()-t1)
 #            t_mean = t_mean/25
-#            MSE_t = mean_squared_error(y_test, y_pred)
-#            R2 = r2_score(y_test, y_pred)
-#            #kolejność kolumn: metoda przycinania, max wzrost MSE, czas przycinania, MSE przed douczaniem, usunięte połączenia, usunięte neurony, MSE po przycinaniu, R2 po przycinaniu, czas predykcji po przycinaniu
+#            f1_t = f1_score(y_test, y_pred, average='macro')
+#            acc = accuracy_score(y_test, y_pred)
+#            #kolejność kolumn: metoda przycinania, max utrata f1, czas przycinania, f1 przed douczaniem, usunięte połączenia, usunięte neurony, dokładność po przycinaniu, f1 po przycinaniu, czas predykcji po przycinaniu
 #            if met in ['SP', 'SPA', 'KP', 'PBV']:
-#                row = f"{met}; {los*100}%; {t_p}; {np.round(MSE_p,6)}; {dele[0]}; {dele[1]}; {np.round(MSE_t,6)}; {np.round(R2,3)}; {np.round(t_mean,6)}s \n"
+#                row = f"{met}; {los*100}%; {t_p}; {np.round(f1_p,3)}; {dele[0]}; {dele[1]}; {np.round(acc,3)}; {np.round(f1_t,3)}; {np.round(t_mean,6)}s \n"
 #            else:
-#                row = f"{met}; {los*100}%; {t_p}; {np.round(MSE_p,6)}; -; {dele}; {np.round(MSE_t,6)}; {np.round(R2,3)}; {np.round(t_mean,6)}s \n"
+#                row = f"{met}; {los*100}%; {t_p}; {np.round(f1_p,3)}; -; {dele}; {np.round(acc,3)}; {np.round(f1_t,3)}; {np.round(t_mean,6)}s \n"
 #            f = open(RESULT_FOLDER+f"raw_txt/{data[data_number]}_network_{l_n}.txt", 'a')
 #            f.write(row)
 #            f.close()
+#            #generowanie macierzy konfuzji
+#            cls_names = clf_t.class_labels_ #nazwy klas w zbiorze
+#            conf_matrix = pd.DataFrame(confusion_matrix(y_test, y_pred, labels=cls_names), index=cls_names, columns=cls_names)
+#            column_format = "|c|" + "".join(["c"]*conf_matrix.shape[0]) + "|"
+#            conf_matrix.to_latex(RESULT_FOLDER+f"conf_matrix/{data[data_number]}_network_{l_n}_pruned_{met}_los_{los}.txt", column_format=column_format)
+
+##dla regresji
+for network_number in range(3): #pętla po architekturach sieci
+    print("Architektura", networks_neurons[network_number])
+    l_n = str(networks_neurons[network_number]) if type(networks_neurons[network_number]) == int else '-'.join(np.array(networks_neurons[network_number], dtype=str))
+    [reg] = unpickle_all(NETWORK_FOLDER+f"{data[data_number]}_network_{l_n}.bin") #odczyt sieci z pliku .bin
+
+    for met in methods:
+        if met == 'APERTP': #metoda działa identycznie do APERT dla regresji, więc zostaje pominięta
+            continue
+        for los in [0, 0.5, 1, 1.75, 2.5]:
+            reg_t = copy.deepcopy(reg)
+            dele, MSE_p, t_p = methods[met](reg_t, los, X_train, y_train, X_v=X_val, y_v=y_val, ep=50)
+            pickle_all(PRUNE_NET_FOLDER+f"{data[data_number]}_network_{l_n}_pruned_{met}_los_{los}", [reg_t])
+            print(met, los, dele, MSE_p, t_p)
+            t_mean = 0 #średni czas predykcji
+            for _ in range(25):
+                t1 = time.time()
+                y_pred = reg_t.predict(X_test)
+                t_mean += (time.time()-t1)
+            t_mean = t_mean/25
+            MSE_t = mean_squared_error(y_test, y_pred)
+            R2 = r2_score(y_test, y_pred)
+            #kolejność kolumn: metoda przycinania, max wzrost MSE, czas przycinania, MSE przed douczaniem, usunięte połączenia, usunięte neurony, MSE po przycinaniu, R2 po przycinaniu, czas predykcji po przycinaniu
+            if met in ['SP', 'SPA', 'KP', 'PBV']:
+                row = f"{met}; {los*100}%; {t_p}; {np.round(MSE_p,6)}; {dele[0]}; {dele[1]}; {np.round(MSE_t,6)}; {np.round(R2,3)}; {np.round(t_mean,6)}s \n"
+            else:
+                row = f"{met}; {los*100}%; {t_p}; {np.round(MSE_p,6)}; -; {dele}; {np.round(MSE_t,6)}; {np.round(R2,3)}; {np.round(t_mean,6)}s \n"
+            f = open(RESULT_FOLDER+f"raw_txt/{data[data_number]}_network_{l_n}.txt", 'a')
+            f.write(row)
+            f.close()
 
 
-
-
-
-#TEST DZIAŁANIA - DO PÓŹNIEJSZEGO USUNIĘCIA
-##data = pd.read_csv(RAW_DATA_FOLDER+"iris.data", header=None)
-##X = normalize(data.iloc[:,0:4].values, norm="max", axis=0)
-##Y = data.iloc[:,4].values
-##X_train, X_test_val, y_train, y_test_val = train_test_split(X, Y, train_size=0.6)
-##X_test, X_val, y_test, y_val = train_test_split(X_test_val, y_test_val, test_size=0.25)
-
-##pickle_all(DATA_FOLDER+"iris_data.bin", [X_train, X_test, X_val, y_train, y_test, y_val])
-#[X_train, X_test, X_val, y_train, y_test, y_val] = unpickle_all(DATA_FOLDER+"iris_data.bin")
-
-#clf = myMLP.Classifier(epochs=100, activation="relu", hidden=(9,5,4))
-#clf.fit(X_train, y_train, X_val, y_val)
-
-##pickle_all(NETWORK_FOLDER+"iris.bin", [clf])
-#[clf] = unpickle_all(NETWORK_FOLDER+"iris.bin")
-
-##print(clf.coefs_)
-#print("Sieć bez przycięcia:")
-#print("F1 train: ", f1_score(y_train, clf.predict(X_train), average='macro'))
-#print("F1 test: ", f1_score(y_test, clf.predict(X_test), average='macro'))
-#print("F1 validation ", f1_score(y_val, clf.predict(X_val), average='macro'))
-#print()
-
-#ll = 0.05
-
-#print("simple_pruning:")
-#clf1 = copy.deepcopy(clf)
-#a, d1, t1 = prune.simple_pruning(clf1, ll, X_train, y_train, X_v=X_val, y_v=y_val, refit=False)
-#print("[Przycięte wagi, neurony]: ", a)
-#print("F1 przed douczaniem", d1)
-#print("Czas przycinania: ", t1)
-##print(clf1.coefs_)
-#print("Architektura po przycinaniu:", clf1.hidden)
-
-#print("F1 train: ", f1_score(y_train, clf1.predict(X_train), average='macro'))
-#print("F1 test: ", f1_score(y_test, clf1.predict(X_test), average='macro'))
-#print("F1 validation ", f1_score(y_val, clf1.predict(X_val), average='macro'))
-#print()
-
-
-#print("simple_pruning_amendment:")
-#clf2 = copy.deepcopy(clf)
-#b, d2, t2 = prune.simple_pruning_amendment(clf2, ll, X_train, y_train, X_v=X_val, y_v=y_val, refit=False)
-#print("[Przycięte wagi, neurony]: ", b)
-#print("F1 przed douczaniem", d2)
-#print("Czas przycinania: ", t2)
-##print(clf2.coefs_)
-#print("Architektura po przycinaniu:", clf2.hidden)
-
-#print("F1 train: ", f1_score(y_train, clf2.predict(X_train), average='macro'))
-#print("F1 test: ", f1_score(y_test, clf2.predict(X_test), average='macro'))
-#print("F1 validation ", f1_score(y_val, clf2.predict(X_val), average='macro'))
-#print()
-
-
-#print("karnin_pruning:")
-#clf3 = copy.deepcopy(clf)
-#c, d3, t3 = prune.karnin_pruning(clf3, ll, X_train, y_train, X_v=X_val, y_v=y_val, refit=False)
-#print("[Przycięte wagi, neurony]: ", c)
-#print("F1 przed douczaniem", d3)
-#print("Czas przycinania: ", t3)
-##print(clf3.coefs_)
-#print("Architektura po przycinaniu:", clf3.hidden)
-
-#print("F1 train: ", f1_score(y_train, clf3.predict(X_train), average='macro'))
-#print("F1 test: ", f1_score(y_test, clf3.predict(X_test), average='macro'))
-#print("F1 validation ", f1_score(y_val, clf3.predict(X_val), average='macro'))
-#print()
-
-
-#print("pruning_by_variance:")
-#clf4 = copy.deepcopy(clf)
-#d, d4, t4 = prune.pruning_by_variance(clf4, ll, X_train, y_train, X_v=X_val, y_v=y_val, refit=False)
-#print("[Przycięte wagi, neurony]: ", d)
-#print("F1 przed douczaniem", d4)
-#print("Czas przycinania: ", t4)
-##print(clf4.coefs_)
-#print("Architektura po przycinaniu:", clf4.hidden)
-
-#print("F1 train: ", f1_score(y_train, clf4.predict(X_train), average='macro'))
-#print("F1 test: ", f1_score(y_test, clf4.predict(X_test), average='macro'))
-#print("F1 validation ", f1_score(y_val, clf4.predict(X_val), average='macro'))
-#print()
-
-
-#print("FBI_pruning:")
-#clf5 = copy.deepcopy(clf)
-#e, d5, t5 = prune.FBI_pruning(clf5, ll, X_train, y_train, X_v=X_val, y_v=y_val)
-#print("Przycięte neurony:", e)
-#print("F1 przed douczaniem", d5)
-#print("Czas przycinania: ", t5)
-##print(clf5.coefs_)
-#print("Architektura po przycinaniu:", clf5.hidden)
-
-#print("F1 train: ", f1_score(y_train, clf5.predict(X_train), average='macro'))
-#print("F1 test: ", f1_score(y_test, clf5.predict(X_test), average='macro'))
-#print("F1 validation ", f1_score(y_val, clf5.predict(X_val), average='macro'))
-#print()
-
-
-#print("APERT_pruning:")
-#clf6 = copy.deepcopy(clf)
-#f, d6, t6 = prune.APERT_pruning(clf6, ll, X_train, y_train, X_v=X_val, y_v=y_val)
-#print("Przycięte neurony:", f)
-#print("F1 przed douczaniem", d6)
-#print("Czas przycinania: ", t6)
-##print(clf6.coefs_)
-#print("Architektura po przycinaniu:", clf6.hidden)
-
-#print("F1 train: ", f1_score(y_train, clf6.predict(X_train), average='macro'))
-#print("F1 test: ", f1_score(y_test, clf6.predict(X_test), average='macro'))
-#print("F1 validation ", f1_score(y_val, clf6.predict(X_val), average='macro'))
-#print()
-
-
-#print("APERTP_pruning:")
-#clf7 = copy.deepcopy(clf)
-#g, d7, t7 = prune.APERTP_pruning(clf7, ll, X_train, y_train, X_v=X_val, y_v=y_val)
-#print("Przycięte neurony:", g)
-#print("F1 przed douczaniem", d7)
-#print("Czas przycinania: ", t7)
-##print(clf7.coefs_)
-#print("Architektura po przycinaniu:", clf7.hidden)
-
-#print("F1 train: ", f1_score(y_train, clf7.predict(X_train), average='macro'))
-#print("F1 test: ", f1_score(y_test, clf7.predict(X_test), average='macro'))
-#print("F1 validation ", f1_score(y_val, clf7.predict(X_val), average='macro'))
-#print()
-
-
-#print("PD_pruning:")
-#clf8 = copy.deepcopy(clf)
-#h, d8, t8 = prune.PD_pruning(clf8, ll, X_train, y_train, X_v=X_val, y_v=y_val)
-#print("Przycięte neurony:", h)
-#print("F1 przed douczaniem", d8)
-#print("Czas przycinania: ", t8)
-##print(clf8.coefs_)
-#print("Architektura po przycinaniu:", clf8.hidden)
-
-#print("F1 train: ", f1_score(y_train, clf8.predict(X_train), average='macro'))
-#print("F1 test: ", f1_score(y_test, clf8.predict(X_test), average='macro'))
-#print("F1 validation ", f1_score(y_val, clf8.predict(X_val), average='macro'))
-#print()
-
-
-#print("PEB_pruning:")
-#clf9 = copy.deepcopy(clf)
-#i, d9, t9 = prune.PEB_pruning(clf9, ll, X_train, y_train, X_v=X_val, y_v=y_val)
-#print("Przycięte neurony:", i)
-#print("F1 przed douczaniem", d9)
-#print("Czas przycinania: ", t9)
-##print(clf9.coefs_)
-#print("Architektura po przycinaniu:", clf9.hidden)
-
-#print("F1 train: ", f1_score(y_train, clf9.predict(X_train), average='macro'))
-#print("F1 test: ", f1_score(y_test, clf9.predict(X_test), average='macro'))
-#print("F1 validation ", f1_score(y_val, clf9.predict(X_val), average='macro'))
-#print()
-
-
-
-#x = np.sort(np.random.uniform(-2,2,250)).reshape(-1,1)
-#y = 2*x + 1
-
-#reg = myMLP.Regressor(activation="sigmoid")
-#reg.fit(x,y)
-##print(reg.coefs_)
-#print(mean_squared_error(y, reg.predict(x)))
-
-#reg1 = copy.deepcopy(reg)
-#aa, dd1, tt1 = prune.FBI_pruning(reg1, 1, x, y)
-#print(aa)
-#print(dd1)
-#print(tt1)
-#print(reg1.hidden)
-
-#print(mean_squared_error(y, reg1.predict(x)))
