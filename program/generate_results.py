b2b7a16bec9fb81c047ed90a3b0e87d49a7a7366
Mateusz Kupidura <m.kupidura@gazeta.pl>
2023-05-23 13:34:05 +0200

    Poprawa tytułów na wykresach, była literówka

--- program/generate_results.py
+++ program/generate_results.py
@@ -28,7 +28,7 @@ data = ["rice", "anuran_family", "anuran_genus", "anuran_species", "dry_bean",
 networks_neurons = [(30), (18,15), (16,13,10)]
 
 data_number = 9
-network_number = 0
+network_number = 1
 
 
 # dok_f1_table = []
@@ -49,27 +49,27 @@ network_number = 0
 # df.to_csv(RESULT_FOLDER+"klasyfikacje.csv")
 # df.to_latex(RESULT_FOLDER+"klasyfikacje.txt", column_format="|c|c|c|c|c|c|")
 
-mse_r2_table = []
-for data_number in [5,8,9]:
-        [X_train, X_test, X_val, y_train, y_test, y_val] = unpickle_all(DATA_FOLDER+f"{data[data_number]}_data.bin")
-        for network_number in range(3):
-            l_n = str(networks_neurons[network_number]) if type(networks_neurons[network_number]) == int else '-'.join(np.array(networks_neurons[network_number], dtype=str))
-            [clf] = unpickle_all(NETWORK_FOLDER+f"{data[data_number]}_network_{l_n}.bin")
-            y_pred_train = clf.predict(X_train)
-            y_pred_test = clf.predict(X_test)
-            acc_train = np.round(mean_squared_error(y_train, y_pred_train), 6)
-            f1_train = np.round(r2_score(y_train, y_pred_train), 3)
-            acc_test = np.round(mean_squared_error(y_test, y_pred_test), 6)
-            f1_test = np.round(r2_score(y_test, y_pred_test), 3)
-            mse_r2_table.append([data[data_number], l_n, acc_train, f1_train, acc_test, f1_test])
-df = pd.DataFrame(mse_r2_table, columns=["Zbiór", "Architektura", "mse_ucz", "R^{2}_ucz", "mse_test", "R^{2}_test"])
-df.set_index(["Zbiór", "Architektura"], inplace=True)
-df.to_csv(RESULT_FOLDER+"regresje.csv")
-df.to_latex(RESULT_FOLDER+"regresje.txt", column_format="|c|c|c|c|c|c|")
+# mse_r2_table = []
+# for data_number in [5,8,9]:
+#         [X_train, X_test, X_val, y_train, y_test, y_val] = unpickle_all(DATA_FOLDER+f"{data[data_number]}_data.bin")
+#         for network_number in range(3):
+#             l_n = str(networks_neurons[network_number]) if type(networks_neurons[network_number]) == int else '-'.join(np.array(networks_neurons[network_number], dtype=str))
+#             [clf] = unpickle_all(NETWORK_FOLDER+f"{data[data_number]}_network_{l_n}.bin")
+#             y_pred_train = clf.predict(X_train)
+#             y_pred_test = clf.predict(X_test)
+#             acc_train = np.round(mean_squared_error(y_train, y_pred_train), 6)
+#             f1_train = np.round(r2_score(y_train, y_pred_train), 3)
+#             acc_test = np.round(mean_squared_error(y_test, y_pred_test), 6)
+#             f1_test = np.round(r2_score(y_test, y_pred_test), 3)
+#             mse_r2_table.append([data[data_number], l_n, acc_train, f1_train, acc_test, f1_test])
+# df = pd.DataFrame(mse_r2_table, columns=["Zbiór", "Architektura", "mse_ucz", "R^{2}_ucz", "mse_test", "R^{2}_test"])
+# df.set_index(["Zbiór", "Architektura"], inplace=True)
+# df.to_csv(RESULT_FOLDER+"regresje.csv")
+# df.to_latex(RESULT_FOLDER+"regresje.txt", column_format="|c|c|c|c|c|c|")
 
 
 
-'''
+
 methods = ['SP', 'SPA', 'KP', 'PBV', 'FBI', 'APERT', 'APERTP', 'PD', 'PEB']
 
 l_n = str(networks_neurons[network_number]) if type(networks_neurons[network_number]) == int else '-'.join(np.array(networks_neurons[network_number], dtype=str))
@@ -79,7 +79,7 @@ for met in methods:
     if met == 'APERTP': #metoda działa identycznie do APERT dla regresji, więc zostaje pominięta
         continue
     for los in [0, 0.5, 1, 1.75, 2.5]:
-    # for los in [0, 0.025, 0.05, 0.075, 0.1]:
+    #for los in [0, 0.025, 0.05, 0.075, 0.1]:
         [siec] =  unpickle_all(PRUNE_NET_FOLDER+f"{data[data_number]}_network_{l_n}_pruned_{met}_los_{los}")
         arch_po_przy.append(str(siec.hidden) if type(siec.hidden) == int else '-'.join(np.array(siec.hidden, dtype=str)))
 
@@ -108,7 +108,7 @@ for i in np.unique(df['Metoda']):
 plt.legend()
 plt.xlabel("Maksymalna utrata jakości")
 plt.ylabel("Usunięte neurony")
-plt.title("Usunięte neurony w zależnoci od utraty jakości")
+plt.title("Usunięte neurony w zależnśoci od utraty jakości")
 plt.savefig(RESULT_FOLDER+f"plots/UN_{data[data_number]}_network_{l_n}.png")
 
 plt.figure(figsize=(16,9))
@@ -118,10 +118,10 @@ for i in np.unique(df['Metoda']):
 plt.legend()
 plt.xlabel("Maksymalna utrata jakości")
 plt.ylabel("Czas trwania przycinania [s]")
-plt.title("Czas przycinania w zależnoci od utraty jakości")
+plt.title("Czas przycinania w zależności od utraty jakości")
 plt.savefig(RESULT_FOLDER+f"plots/CP_{data[data_number]}_network_{l_n}.png")
 
-
+'''
 #Generowanie tabeli latex
 sorterIndex = dict(zip(methods, range(len(methods))))
 df['met_sort'] = df["Metoda"].map(sorterIndex)
